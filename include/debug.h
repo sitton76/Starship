4e9cd2481373c1a9010ddd1d038445871716a2b8
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-10-17 01:59:18 -0300

    macros

--- include/debug.h
+++ include/debug.h
@@ -1,6 +1,10 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#define gEXMatrixGroupDecomposed(...)
+#define gEXMatrixGroupDecomposedNormal(...)
+#define gEXPopMatrixGroup(...)
+
 #define DEBUG_SPEED_CONTROL 1
 
 #endif
