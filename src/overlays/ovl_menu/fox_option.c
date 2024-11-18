b01825576cb5117afa5b95b72e7a10b977534500
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-11-18 01:19:29 -0300

    Ranking border fix

--- src/overlays/ovl_menu/fox_option.c
+++ src/overlays/ovl_menu/fox_option.c
@@ -2255,7 +2255,9 @@ void Option_80197914(void) {
     for (i = 0, vec1 = D_menu_801AF100, vec2 = D_menu_801AF118; i < 2; i++, vec1++, vec2++) {
         Matrix_Push(&gGfxMatrix);
         Matrix_Translate(gGfxMatrix, vec1->x, vec1->y, -500.0f, MTXF_APPLY);
-        Matrix_Scale(gGfxMatrix, vec2->x, vec2->y, 1.0f, MTXF_APPLY);
+
+        // @port: Increase the scale by 2.5f to compensate for missing borders
+        Matrix_Scale(gGfxMatrix, vec2->x * 4, vec2->y + 2.5f, 1.0f, MTXF_APPLY);
         Matrix_SetGfxMtx(&gMasterDisp);
         gSPDisplayList(gMasterDisp++, D_menu_801AEF30);
         Matrix_Pop(&gGfxMatrix);
