0f8e848232d1ec418cda0e7b6af478f9bb9114d6
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-04 23:29:56 -0300

    correction

--- src/port/ui/ImguiUI.cpp
+++ src/port/ui/ImguiUI.cpp
@@ -473,8 +473,8 @@ void DrawEnhancementsMenu() {
                 .tooltip = "Restores the beta coin that got replaced with the gold ring"
             });
 
-            UIWidgets::CVarCheckbox("Beta: Restore old boost gauge", "gRestoreOldBoostGauge", {
-                .tooltip = "Restores the old boost gauge that was seen on some beta footage"
+            UIWidgets::CVarCheckbox("Beta: Restore old boost/brake gauge", "gRestoreOldBoostGauge", {
+                .tooltip = "Restores the old boost gauge that was seen in some beta footage"
             });
 
             ImGui::EndMenu();
